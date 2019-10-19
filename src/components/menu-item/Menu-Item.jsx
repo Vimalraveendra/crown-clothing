883a66b72f2a27e86fb94d0ca7533093bcbd6ccd
vimal Raveendran <vimalraveendra@gmail.com>
2019-10-19 20:07:44 +0200

    updated App styles & menu-item sytles

--- src/components/menu-item/Menu-Item.jsx
+++ src/components/menu-item/Menu-Item.jsx
@@ -10,7 +10,7 @@ const MenuItem= ({ title,imageUrl,linkUrl,size})=>{
         }}
         className={`menu-item ${size || ''} `}>
                   <div className="content">
-                      <h1 className="title">{title}</h1>
+                      <h1 className="title">{ title.toUpperCase() }</h1>
                         <span className="sub-title">SHOP NOW</span>
                   </div>
                </div>
