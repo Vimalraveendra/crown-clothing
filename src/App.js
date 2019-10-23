c67081527dd1183a583a060049a081bdda982bef
vimal Raveendran <vimalraveendra@gmail.com>
2019-10-23 18:04:28 +0200

    Created Header component and svg file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shoppage/ShopPage";
-import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
