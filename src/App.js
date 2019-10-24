b8079d0293d5f6484aa2fcc6b6e8cf7f4b655561
vimal Raveendran <vimalraveendra@gmail.com>
2019-10-24 16:03:17 +0200

    created sigin-and-signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shoppage/ShopPage";
 import Header from "./components/header/Header";
+import SignInAndSignUpPage from "./pages/signin-and-signup/SignIn-and-SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/SignIn-and-SignUp.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SignIn-and-SignUp.scss";
+
+const SignInAndSignUpPage = () => {
+  return <div className="signin-and-signup">Sign In</div>;
+};
+
+export default SignInAndSignUpPage;
