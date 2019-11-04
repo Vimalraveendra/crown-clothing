98b6d5aae3d9e24e71c70c9382a23001f79333bc
vimal Raveendran <vimalraveendra@gmail.com>
2019-11-04 18:08:41 +0100

    created SignUp component

--- src/pages/signin-and-signup/SignIn-and-SignUp.jsx
+++ src/pages/signin-and-signup/SignIn-and-SignUp.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SignIn-and-SignUp.scss";
 import SignIn from "../../components/signin/SignIn";
+import SignUp from "../../components/signup/SignUp";
+
 const SignInAndSignUpPage = () => {
   return (
     <div className="signin-and-signup">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
