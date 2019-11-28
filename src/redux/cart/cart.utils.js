aee17546cecdeb36665a00a8a9208b73c80b671c
vimal Raveendran <vimalraveendra@gmail.com>
2019-11-28 20:40:07 +0100

    updated shopData with newData,updated cart utils to group items in cartReducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  //cartItemTOAdd = action.payload from the cartreducer
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
