02d8296973bbb351e041bfa3a589ce2ce627286e
vimal Raveendran <vimalraveendra@gmail.com>
2019-12-28 22:16:28 +0100

    updated the App component with shopdata to camelCase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Homepage from "./pages/homepage/Homepage";
-import ShopPage from "./pages/shoppage/ShopPage";
+import ShopPage from "./pages/Shoppage/ShopPage";
 import Header from "./components/header/Header";
 import SignInAndSignUpPage from "./pages/signin-and-signup/SignIn-and-SignUp";
 import CheckOutPage from "./pages/checkout/CheckOut";
