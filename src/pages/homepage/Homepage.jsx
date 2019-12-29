9f212b5af450cbf3ff0be104c36f3af1dba95ce5
vimal Raveendran <vimalraveendra@gmail.com>
2019-12-29 11:56:10 +0100

    removing redux-logger from production build

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Homepage.scss';
-import Directory from '../../components/directory/Directory';
-const Homepage = ()=>{
-    return(
-        <div className="homepage">
-             <Directory />
-        </div>
-    )
-}
+import React from "react";
+import "./Homepage.scss";
+import Directory from "../../components/directory/Directory";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
 
-export default Homepage; 
+export default Homepage;
