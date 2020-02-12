51a65cb9d9cae8c23f12a9b51e90b36e47d5ca2a
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-12 15:32:01 +0100

    created server and seperated the components to frond end and backend

---
+++ client/src/pages/collection/Collection.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import "./Collection.scss";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selector";
+import CollectionItem from "../../components/collection-items/Collection-Items";
+import { firestore } from "firebase";
+
+const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log("I am subscribing");
+    const unsubscribeFromCollections = firestore
+      .collection("collections")
+      .onSnapshot(snapshot => console.log(snapshot));
+
+    return () => {
+      console.log("I am unsubscribing");
+      unsubscribeFromCollections();
+    };
+  }, []);
+
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
