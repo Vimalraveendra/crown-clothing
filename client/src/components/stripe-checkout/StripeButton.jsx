1c87b6abd8207841cdd11a43c029c9b792f58437
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-13 22:30:26 +0100

    updated the shoppage component

--- client/src/components/stripe-checkout/StripeButton.jsx
+++ client/src/components/stripe-checkout/StripeButton.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_665QtuQOEgqMXFyUsTYDkcZ000v6ypFicF";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert("Payment was successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment.Please make sure you provided credit card."
+        );
+      });
   };
+
   return (
     <StripeCheckout
       label="Pay Now"
--- client/src/pages/collection/Collection.jsx
+++ client/src/pages/collection/Collection.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Collection.scss";
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selector";
 import CollectionItem from "../../components/collection-items/Collection-Items";
-import { firestore } from "firebase";
+// import { firestore } from "../../firebase/Firebase.Utils";
 
 const CollectionPage = ({ collection }) => {
-  useEffect(() => {
-    console.log("I am subscribing");
-    const unsubscribeFromCollections = firestore
-      .collection("collections")
-      .onSnapshot(snapshot => console.log(snapshot));
+  // useEffect(() => {
+  //   console.log("I am subscribing");
+  //   const unsubscribeFromCollections = firestore
+  //     .collection("collections")
+  //     .onSnapshot(snapshot => console.log(snapshot));
 
-    return () => {
-      console.log("I am unsubscribing");
-      unsubscribeFromCollections();
-    };
-  }, []);
+  //   return () => {
+  //     console.log("I am unsubscribing");
+  //     unsubscribeFromCollections();
+  //   };
+  // }, []);
 
   const { title, items } = collection;
   return (
